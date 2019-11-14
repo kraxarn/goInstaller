8f49f744b082b88b029252de850f595e51372c48
kraxarn <me@kraxarn.com>
2019-11-15 00:46:51 +0100

    Update license

--- licenses.go
+++ licenses.go
@@ -28,21 +28,29 @@ const licenses =
 		"THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n" +
 		"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE\n" +
 		"OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n" +
-		"ui\n" +
-		"Copyright (c) 2014 Pietro Gagliardi\n" +
-		"Permission is hereby granted, free of charge, to any person obtaining a copy of this\n" +
-		"software and associated documentation files (the \"Software\"), to deal in the Software\n" +
-		"without restriction, including without limitation the rights to use, copy, modify, merge,\n" +
-		"publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons\n" +
-		"to whom the Software is furnished to do so, subject to the following conditions:\n" +
-		"The above copyright notice and this permission notice shall be included in all copies or\n" +
-		"substantial portions of the Software.\n" +
-		"THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING\n" +
-		"BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\n" +
-		"NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,\n" +
-		"DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
-		"OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n" +
-		"(this is called the MIT License or Expat License; see http://www.opensource.org/licenses/MIT)\n\n" +
+		"fyne:\n" +
+		"Copyright (C) 2018 Fyne.io developers (see AUTHORS)\n" +
+		"All rights reserved.\n\n" +
+		"Redistribution and use in source and binary forms, with or without\n" +
+		"modification, are permitted provided that the following conditions are met:\n" +
+		"* Redistributions of source code must retain the above copyright\n" +
+		"notice, this list of conditions and the following disclaimer.\n" +
+		"* Redistributions in binary form must reproduce the above copyright\n" +
+		"notice, this list of conditions and the following disclaimer in the\n" +
+		"documentation and/or other materials provided with the distribution.\n" +
+		"* Neither the name of Fyne.io nor the names of its contributors may be\n" +
+		"used to endorse or promote products derived from this software without\n" +
+		"specific prior written permission.\n\n" +
+		"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND\n" +
+		"ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED\n" +
+		"WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE\n" +
+		"DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY\n" +
+		"DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES\n" +
+		"(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;\n" +
+		"LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND\n" +
+		"ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT\n" +
+		"(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS\n" +
+		"SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.\n\n" +
 		"grab:\n" +
 		"Copyright (c) 2017 Ryan Armstrong. All rights reserved.\n\n" +
 		"Redistribution and use in source and binary forms, with or without modification,\n" +
